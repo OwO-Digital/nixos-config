815804d84a72c54a12e332d574a90e67d86a8308
Aproxia-dev <apro@r4ilax.eu>
2023-02-06 10:55:40 +0100

    fix: olympus not building

--- overlays/olympus.nix
+++ overlays/olympus.nix
@@ -1,28 +1,28 @@
 { inputs, ... }:
-let
-	pkgOlympus = prev.stdenv.mkDerivation rec {
-		pname = "olympus";
-		version = "2681";
+final: prev:
+	let
+		pkgOlympus = prev.stdenv.mkDerivation rec {
+			pname = "olympus";
+			version = "2681";
 
-		# https://everestapi.github.io/
-		src = prev.fetchzip {
-			url = "https://dev.azure.com/EverestAPI/Olympus/_apis/build/builds/${version}/artifacts?artifactName=linux.main&$format=zip#linux.main.zip";
-			hash = "sha256-r+Zqby5q4FfLaCCu56WW8i6y1GCUFyXeTCRoCEPbtEY=";
+			# https://everestapi.github.io/
+			src = prev.fetchzip {
+				url = "https://dev.azure.com/EverestAPI/Olympus/_apis/build/builds/${version}/artifacts?artifactName=linux.main&$format=zip#linux.main.zip";
+				hash = "sha256-r+Zqby5q4FfLaCCu56WW8i6y1GCUFyXeTCRoCEPbtEY=";
+			};
+
+			buildInputs = [ prev.unzip ];
+			installPhase = ''
+				mkdir -p "$out/opt/olympus/"
+				mv dist.zip "$out/opt/olympus/" && cd "$out/opt/olympus/"
+				unzip dist.zip && rm dist.zip
+				mkdir $out && echo XDG_DATA_HOME=$out
+				echo y | XDG_DATA_HOME="$out/share/" bash install.sh
+				sed -i "/ldconfig/d" ./love && rm ./usr/lib/libSDL2-2.0.so.0
+				sed -i "s/Exec=.*/Exec=olympus %u/g" ../../share/applications/Olympus.desktop
+			'';
 		};
-
-		buildInputs = [ pkgs.unzip ];
-		installPhase = ''
-			mkdir -p "$out/opt/olympus/"
-			mv dist.zip "$out/opt/olympus/" && cd "$out/opt/olympus/"
-			unzip dist.zip && rm dist.zip
-			mkdir $out && echo XDG_DATA_HOME=$out
-			echo y | XDG_DATA_HOME="$out/share/" bash install.sh
-			sed -i "/ldconfig/d" ./love && rm ./usr/lib/libSDL2-2.0.so.0
-			sed -i "s/Exec=.*/Exec=olympus %u/g" ../../share/applications/Olympus.desktop
-		'';
-	};
-in
-final: prev: {
+	in {
 	olympus = prev.buildFHSUserEnv {
 		name = "olympus";
 		runScript = "${pkgOlympus}/opt/olympus/olympus";
