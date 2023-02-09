d8983ef63a72479071067e18b5a5a91f069586a7
Aproxia-dev <apro@r4ilax.eu>
2023-02-09 12:07:09 +0100

    fix(apro): VSCodium config

--- users/apro/programs/apps/vscodium.nix
+++ users/apro/programs/apps/vscodium.nix
@@ -1,60 +1,48 @@
-{
-	config,
-	pkgs,
-  lib,
-	...
-}: {
+{ config, pkgs, lib, ... }: {
+
 	programs.vscode = {
 		enable = true;
 		package = pkgs.master.vscodium;
 
-    userSettings = {
-    	"editor" = {
-        "fontFamily" = "'Iosevka Nerd Font'";
-    	  "cursorBlinking" = "smooth";
-    	  "insertSpaces" = false;
-    	  "detectIndentation" = false;
-    	  "tabSize" = 4;
-    	  "minimap.enabled" = false;
-      };
+		userSettings = {
+			"editor.fontFamily" = "'Iosevka Nerd Font'";
+			"editor.cursorBlinking" = "smooth";
+			"editor.insertSpaces" = false;
+			"editor.detectIndentation" = false;
+			"editor.tabSize" = 4;
+			"editor.minimap.enabled" = false;
 
+			"terminal.integrated.cursorStyle" = "underline";
+			"terminal.integrated.cursorWidth" = 2;
+			"terminal.integrated.fontFamily" = "Iosevka Nerd Font";
+			"terminal.integrated.tabs.defaultIcon" = "heart-filled";
 
-    	"terminal" = {
-        "integrated.cursorStyle" = "underline";
-    	  "integrated.cursorWidth" = 2;
-    	  "integrated.fontFamily" = "Iosevka Nerd Font";
-    	  "integrated.tabs.defaultIcon" = "heart-filled";
-      };
+			"workbench.colorTheme" = "Everblush";
+			"workbench.productIconTheme" = "material-product-icons";
+			"workbench.iconTheme" = "file-icons";
 
-    	"workbench" = {
-        "colorTheme" = "Everblush";
-        "productIconTheme" = "material-product-icons";
-        "iconTheme" = "file-icons";
-      };
+			"window.menuBarVisibility" = "compact";
+			"window.zoomLevel" = 1;
 
-    	"window" = {
-        "menuBarVisibility" = "compact";
-    	  "zoomLevel" = 1;
-      };
-
-    	"nix.serverPath" = "nil";
-    	"git.confirmSync" = false;
-    };
+			"nix.serverPath" = "nil";
+			"git.confirmSync" = false;
+		};
 
 		extensions = (with pkgs.vscode-extensions; [
-			  pkief.material-product-icons
-			  file-icons.file-icons
-			  mkhl.direnv
-			  sumneko.lua
-			  vscodevim.vim
-			  jnoortheen.nix-ide
-			  catppuccin.catppuccin-vsc
-    ]) ++ 
-    (with pkgs.master.vscode-extensions; [
-        ms-python.python
-    ]) ++
-    (with pkgs.everblush; [
-			  vscode-theme
-    ]);
+			pkief.material-product-icons
+			file-icons.file-icons
+			mkhl.direnv
+			sumneko.lua
+			vscodevim.vim
+			jnoortheen.nix-ide
+			catppuccin.catppuccin-vsc
+			editorconfig.editorconfig
+		])
+		++ (with pkgs.master.vscode-extensions; [
+			ms-python.python
+		])
+		++ (with pkgs.everblush; [
+			vscode-theme
+		]);
 	};
 }
