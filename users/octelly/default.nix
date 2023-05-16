769e3a0ea4fe0eeb01169d50e3c0fac9081e5d38
Octelly <eli@stefek.cz>
2023-05-16 18:50:30 +0200

    user folder, vscode, chromium, awesome, kms

--- users/octelly/default.nix
+++ users/octelly/default.nix
@@ -1,232 +1,233 @@
 { config, inputs, lib, pkgs, system, ... }:
 
-{ 
-	home = {
-		packages = with pkgs; [
-			pavucontrol
-			pulsemixer
-			maple-mono-NF
-			ranger
-			cinnamon.nemo-with-extensions
-			firefox
-			armcord
+{
+  home = {
+    packages = with pkgs; [
+      pavucontrol
+      pulsemixer
+      maple-mono-NF
+      ranger
+      cinnamon.nemo-with-extensions
+      firefox
+      armcord
 
-			rnix-lsp
-			nixpkgs-fmt
+      rnix-lsp
+      nixpkgs-fmt
 
-			networkmanagerapplet
-			networkmanager-l2tp
+      networkmanagerapplet
+      networkmanager-l2tp
 
-			libgnome-keyring
-		];
-		keyboard = {
-			layout = "us,cz(qwerty)";
-			options = "grp:win_space_toggle";
-		};
-	};
+      libgnome-keyring
+    ];
+    keyboard = {
+      layout = "us,cz(qwerty)";
+      options = "grp:win_space_toggle";
+    };
+  };
 
-	fonts.fontconfig.enable = true;
+  fonts.fontconfig.enable = true;
 
-	programs.chromium = {
-		enable = true;
-		package = (pkgs.chromium.override {
-			ungoogled = true;
-			channel = "ungoogled-chromium";
-		});
-		commandLineArgs = [
-			"--force-dark-mode"
-			"--enable-features=WebUIDarkMode"
-			"--disable-features=ClearDataOnExit"
-			"--ozone-platform-hint=auto"  # automatic wayland
-			"--remove-tabsearch-button"
-			"--show-avatar-button=never"
-		];
-		extensions = [
-			{ id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; }  # uBlock Origin
-			{ id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; }  # Dark Reader
-			{ id = "fiabciakcmgepblmdkmemdbbkilneeeh"; }  # Tab Suspender
-			{ id = "gebbhagfogifgggkldgodflihgfeippi"; }  # Return YouTube Dislike
-			{ id = "mnjggcdmjocbbbhaepdhchncahnbgone"; }  # SponsorBlock
-		];
-	};
+  programs.chromium = {
+    enable = true;
+    package = (pkgs.chromium.override {
+      ungoogled = true;
+      channel = "ungoogled-chromium";
+    });
+    commandLineArgs = [
+      "--force-dark-mode"
+      "--enable-features=WebUIDarkMode"
+      "--disable-features=ClearDataOnExit"
+      "--ozone-platform-hint=auto" # automatic wayland
+      "--remove-tabsearch-button"
+      "--show-avatar-button=never"
+    ];
+    extensions = [
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
+      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
+      { id = "fiabciakcmgepblmdkmemdbbkilneeeh"; } # Tab Suspender
+      { id = "gebbhagfogifgggkldgodflihgfeippi"; } # Return YouTube Dislike
+      { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
+    ];
+  };
 
-	programs.vscode = {
-		enable = true;
-		extensions = (with pkgs.open-vsx; [
-			# make thing usable
-			vscodevim.vim
+  programs.vscode = {
+    enable = true;
+    extensions = (with pkgs.open-vsx; [
+      # make thing usable
+      vscodevim.vim
 
-			# theming
-			sainnhe.sonokai
-			pkief.material-icon-theme
+      # theming
+      sainnhe.sonokai
+      pkief.material-icon-theme
 
-			# language support
-			bungcip.better-toml
-			ms-pyright.pyright
-			ms-python.python
-			ms-python.isort
-			jnoortheen.nix-ide
-			ms-vscode.makefile-tools
-			wayou.vscode-todo-highlight
-		]) ++ (with pkgs.vscode-marketplace; [
-			# language support
-			sumneko.lua
-			vgalaktionov.moonscript
-			tnze.snbt
-			mrmlnc.vscode-json5
-			# this one broken somehow, bruh:
-			#rust-lang.rust-analyzer
+      # language support
+      bungcip.better-toml
+      ms-pyright.pyright
+      ms-python.python
+      ms-python.isort
+      jnoortheen.nix-ide
+      ms-vscode.makefile-tools
+      wayou.vscode-todo-highlight
+    ]) ++ (with pkgs.vscode-marketplace; [
+      # language support
+      sumneko.lua
+      vgalaktionov.moonscript
+      tnze.snbt
+      mrmlnc.vscode-json5
+      # this one broken somehow, bruh:
+      #rust-lang.rust-analyzer
 
-			# GitHub
-			github.remotehub
-			github.vscode-pull-request-github
+      # GitHub
+      github.remotehub
+      github.vscode-pull-request-github
 
-			# remote dev
-			ms-vscode.remote-repositories
-			ms-vscode-remote.remote-ssh
-			ms-vscode-remote.remote-ssh-edit
-		]);
-		userSettings = {
-			# Vim controls
-			"extensions.experimental.affinity" = {
-			    "asvetliakov.vscode-neovim" = 1;
-    			};
-			
-			# theming
-    			"workbench.colorTheme"             = "Sonokai Shusia";
-    			"workbench.iconTheme"              = "material-icon-theme";
-			"material-icon-theme.folder.color" = "#e5c463";
+      # remote dev
+      ms-vscode.remote-repositories
+      ms-vscode-remote.remote-ssh
+      ms-vscode-remote.remote-ssh-edit
+    ]);
+    userSettings = {
+      # Vim controls
+      "extensions.experimental.affinity" = {
+        "asvetliakov.vscode-neovim" = 1;
+      };
 
-    			"window.menuBarVisibility" = "toggle";
+      # theming
+      "workbench.colorTheme" = "Sonokai Shusia";
+      "workbench.iconTheme" = "material-icon-theme";
+      "material-icon-theme.folder.color" = "#e5c463";
 
-    			"editor.fontFamily"        = "'Maple Mono NF', 'Cartograph CF', 'FiraCode Nerd Font Mono', 'monospace', monospace";
-    			"debug.console.fontFamily" = "'Maple Mono NF', 'Cartograph CF', 'FiraCode Nerd Font Mono', 'monospace', monospace";
-    			"editor.fontLigatures"     = "'cv02', 'cv03', 'cv04'";
-    			"editor.fontVariations"    = true;
-    			"editor.fontSize"          = 17;
-    			"debug.console.fontSize"   = 17;
+      "window.menuBarVisibility" = "toggle";
 
-    			"todohighlight.defaultStyle" = {
-    			    "backgroundColor" = "#0000";
-    			    "color"           = "#f85e84";
-    			};
-    			"todohighlight.keywords" = [
-    			    {
-    			        "text"           = "FIXME:";
-    			        "color"          = "#f85e84";
-    			        "backgroundColor"= "#0000";
-    			    }
-    			];
+      "editor.fontFamily" = "'Maple Mono NF', 'Cartograph CF', 'FiraCode Nerd Font Mono', 'monospace', monospace";
+      "debug.console.fontFamily" = "'Maple Mono NF', 'Cartograph CF', 'FiraCode Nerd Font Mono', 'monospace', monospace";
+      "editor.fontLigatures" = "'cv02', 'cv03', 'cv04'";
+      "editor.fontVariations" = true;
+      "editor.fontSize" = 17;
+      "debug.console.fontSize" = 17;
 
-			# terminal
-    			"terminal.external.linuxExec"                             = "kitty";
-    			"terminal.integrated.cursorBlinking"                      = true;
-    			"terminal.integrated.cursorStyle"                         = "line";
-    			"terminal.integrated.defaultProfile.linux"                = "zsh";
-    			"terminal.integrated.drawBoldTextInBrightColors"          = false;
-    			"terminal.integrated.gpuAcceleration"                     = "on";
-    			"terminal.integrated.minimumContrastRatio"                = 1;
-    			"terminal.integrated.shellIntegration.decorationsEnabled" = "never";
-			
-			# Git
-			"githubPullRequests.createOnPublishBranch" = "never";
-			"git.confirmSync" = false;
-			"git.autofetch" = true;
+      "todohighlight.defaultStyle" = {
+        "backgroundColor" = "#0000";
+        "color" = "#f85e84";
+      };
+      "todohighlight.keywords" = [
+        {
+          "text" = "FIXME:";
+          "color" = "#f85e84";
+          "backgroundColor" = "#0000";
+        }
+      ];
 
-			# code formatting
-			"editor.formatOnSave" = true;
-			"python.formatting.provider" = "black";
+      # terminal
+      "terminal.external.linuxExec" = "kitty";
+      "terminal.integrated.cursorBlinking" = true;
+      "terminal.integrated.cursorStyle" = "line";
+      "terminal.integrated.defaultProfile.linux" = "zsh";
+      "terminal.integrated.drawBoldTextInBrightColors" = false;
+      "terminal.integrated.gpuAcceleration" = "on";
+      "terminal.integrated.minimumContrastRatio" = 1;
+      "terminal.integrated.shellIntegration.decorationsEnabled" = "never";
 
-			# brhuhe
-			"workbench.startupEditor" = "none";
-		};
-	};
+      # Git
+      "githubPullRequests.createOnPublishBranch" = "never";
+      "git.confirmSync" = false;
+      "git.autofetch" = true;
 
-	programs.kitty = {
-		enable = true;
-		font = {
-			name = "Maple Mono NF";
-			size = 14;
-		};
-		extraConfig = ''
-			include ./themes/sonokai-shusia.conf
-			background_opacity 0.9
-		'';
-	};
+      # code formatting
+      "editor.formatOnSave" = true;
+      "python.formatting.provider" = "black";
 
-	programs.zsh = {
-		enable = true;
-		enableAutosuggestions = true;
-		enableSyntaxHighlighting = true;
-		enableVteIntegration = true;
-		autocd = true;
-		historySubstringSearch.enable = true;
-	};
+      # brhuhe
+      "workbench.startupEditor" = "none";
+    };
+  };
 
-	programs.fzf = {
-		enable = true;
-		enableZshIntegration = true;
-	};
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "Maple Mono NF";
+      size = 14;
+    };
+    extraConfig = ''
+      			include ./themes/sonokai-shusia.conf
+      			background_opacity 0.9
+      		'';
+  };
 
-	programs.zoxide = {
-		enable = true;
-		enableZshIntegration = true;
-	};
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    enableVteIntegration = true;
+    autocd = true;
+    historySubstringSearch.enable = true;
+  };
 
-	programs.btop.enable = true;
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 
-	xdg = {
-		enable = true;
-		mime.enable = true;
-		mimeApps = {
-			enable = true;
-			defaultApplications = {
-				"inode/directory" = [ "nemo.desktop" ];
-				"video" = [ "mpv.desktop" ];
-				"audio" = [ "mpv.desktop" ];
-				"text" = [ "neovim.desktop" ];
-			};
-		};
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 
-		userDirs = {
-			enable = true;
-			createDirectories = true;
+  programs.btop.enable = true;
 
-			desktop = null;
-			publicShare = null;
-			templates = null;
+  xdg = {
+    enable = true;
+    mime.enable = true;
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "inode/directory" = [ "nemo.desktop" ];
+        "video" = [ "mpv.desktop" ];
+        "audio" = [ "mpv.desktop" ];
+        "text" = [ "neovim.desktop" ];
+      };
+    };
 
-			download  = "${config.home.homeDirectory}/Downloads";
-			documents = "${config.home.homeDirectory}/Documents";
-			music     = "${config.home.homeDirectory}/Music";
-			pictures  = "${config.home.homeDirectory}/Pictures";
-			videos    = "${config.home.homeDirectory}/Videos";
-		};
+    userDirs = {
+      enable = true;
+      createDirectories = true;
 
-		configFile = {
-			"chromium/policies/managed/extra.json".text = builtins.toJSON {
-				"OsColorMode" = "dark";
-				"WebAppInstallForceList" = [
-					{
-						"default_launch_container" = "window";
-						"url" = "https://www.youtube.com/";
-					}
-				];
-				"SyncDisabled" = true;
-				"PasswordManagerEnabled" = false;
-				"BrowserSignin" = 0;
-				"BrowserGuestModeEnabled" = false;
-				"ClearBrowsingDataOnExitList" = [];
-				"ForceEphemeralProfiles" = false;
+      desktop = null;
+      publicShare = null;
+      templates = null;
 
-				"BookmarkBarEnabled" = false;
-				"BrowserThemeColor" = "#000000";
-				"NewTabPageLocation" = "https://start.johnystar.moe/";
-				"HomepageIsNewTabPage" = true;
-				"ShowHomeButton" = false;
-			};
-			"kitty/themes/sonokai-shusia.conf".source = ./sonokai-shusia.conf;
-		};
-	};
+      download = "${config.home.homeDirectory}/Downloads";
+      documents = "${config.home.homeDirectory}/Documents";
+      music = "${config.home.homeDirectory}/Music";
+      pictures = "${config.home.homeDirectory}/Pictures";
+      videos = "${config.home.homeDirectory}/Videos";
+    };
+
+    configFile = {
+      "awesome".source = ./awesome;
+      "chromium/policies/managed/extra.json".text = builtins.toJSON {
+        "OsColorMode" = "dark";
+        "WebAppInstallForceList" = [
+          {
+            "default_launch_container" = "window";
+            "url" = "https://www.youtube.com/";
+          }
+        ];
+        "SyncDisabled" = true;
+        "PasswordManagerEnabled" = false;
+        "BrowserSignin" = 0;
+        "BrowserGuestModeEnabled" = false;
+        "ClearBrowsingDataOnExitList" = [ ];
+        "ForceEphemeralProfiles" = false;
+
+        "BookmarkBarEnabled" = false;
+        "BrowserThemeColor" = "#000000";
+        "NewTabPageLocation" = "https://start.johnystar.moe/";
+        "HomepageIsNewTabPage" = true;
+        "ShowHomeButton" = false;
+      };
+      "kitty/themes/sonokai-shusia.conf".source = ./sonokai-shusia.conf;
+    };
+  };
 }
 
