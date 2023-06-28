0d683d1bfd9010ae76b9c627f44dc662d57a924c
Octelly <eli@stefek.cz>
2023-06-28 13:06:42 +0200

    olympus doesn't build

--- hosts/ocean-t460/configuration.nix
+++ hosts/ocean-t460/configuration.nix
@@ -1,23 +1,23 @@
 { config, pkgs, ... }: {
 
-	modules = {
-		desktop = {
-			gaming = {
-				steam.enable   = true;
-				olympus.enable = true;
-			};
-			awesome = {
-				enable = true;
-				package = pkgs.awesome-git;
-			};
-		};
-		hardware.laptop.enable = true;
-		system = {
-			sound.enable = true;
-			xorg = {
-				enable = true;
-			};
-		};
-	};
-	networking.firewall.allowedTCPPorts = [ 25565 ];
+  modules = {
+    desktop = {
+      gaming = {
+        steam.enable = true;
+        olympus.enable = false;
+      };
+      awesome = {
+        enable = true;
+        package = pkgs.awesome-git;
+      };
+    };
+    hardware.laptop.enable = true;
+    system = {
+      sound.enable = true;
+      xorg = {
+        enable = true;
+      };
+    };
+  };
+  networking.firewall.allowedTCPPorts = [ 25565 ];
 }
