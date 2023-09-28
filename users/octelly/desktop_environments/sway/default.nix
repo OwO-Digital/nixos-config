a41c5c3074a3d7f7c7ec370307827d6813bdba26
Octelly <eli@stefek.cz>
2023-09-28 21:21:16 +0200

    elly sway update

--- users/octelly/desktop_environments/sway/default.nix
+++ users/octelly/desktop_environments/sway/default.nix
@@ -3,6 +3,17 @@
 }: {
   xdg.configFile.sway.source = ./config;
   xdg.configFile.waybar.source = ./waybar;
+  xdg.configFile."swaync/config.json".text = builtins.toJSON {
+    "control-center-layer" = "top";
+    "keyboard-shortcuts" = false;
+    #"layer-shell" = true;
+    "widgets" = [
+      "mpris"
+      "title"
+      "dnd"
+      "notifications"
+    ];
+  };
 
   wayland.windowManager.sway = {
     #systemd.enable = false;
