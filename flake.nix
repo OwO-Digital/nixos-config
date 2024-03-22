b0ff2d7dda1268899dea584f195dce3045e48229
Octelly (Eli Štefků) <eli@stefek.cz>
2024-03-22 03:09:59 +0100

    flake update and tweaks

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "owo.digital flake";
 
   inputs = {
+    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    # Official hardware config templates
     nixos-hardware.url = "github:nixos/nixos-hardware";
 
     # Do not rename the following input
@@ -12,10 +14,20 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # User-specific options
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Generate documentation for Nix Flakes.
+    snowfall-frost = {
+      url = "github:snowfallorg/frost";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ### PACKAGE SOURCES ###
+    # they would go here :3
   };
 
   outputs = inputs:
@@ -31,6 +43,14 @@
           title = "owo.digital";
         };
       };
+
+      channels-config = {
+        allowUnfree = true;
+      };
+
+      overlays = with inputs; [
+        snowfall-frost.overlays.default
+      ];
 
       system.hosts = {
         ocean-t460.modules = with inputs; [
