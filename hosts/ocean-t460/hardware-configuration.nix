fffa70ff51dd255adf74bcb38cde81849adcad3d
Octelly <eli@stefek.cz>
2024-04-23 21:52:19 +0200

    Steam performance fix

--- hosts/ocean-t460/hardware-configuration.nix
+++ hosts/ocean-t460/hardware-configuration.nix
@@ -39,5 +39,10 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "conservative";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault true;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
 }
