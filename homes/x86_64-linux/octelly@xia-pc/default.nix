3f006c7c4524cd88c28a6588d456a8d3cda115f1
Octelly (Eli Štefků) <eli@stefek.cz>
2024-05-22 04:33:00 +0200

    nixvim experimentation

--- homes/x86_64-linux/octelly@xia-pc/default.nix
+++ homes/x86_64-linux/octelly@xia-pc/default.nix
@@ -41,8 +41,250 @@ with config; {
   };
 
   home.packages = with pkgs; [
-    snowfallorg.frost
+    #snowfallorg.frost
     nil
-    owo-digital.MarioVsLuigi-bin
+    #owo-digital.MarioVsLuigi-bin
+    #eartag
   ];
+
+  programs.tauon = {
+    enable = true;
+  };
+
+  fonts.fontconfig = {
+    #enable = true;
+    defaultFonts = {
+      monospace = [
+        "Maple Mono NF"
+      ];
+    };
+  };
+
+  programs.nixvim = {
+    enable = true;
+    defaultEditor = true;
+
+    extraPlugins = with pkgs.vimPlugins; [
+      sonokai
+    ];
+
+    globals = {
+      sonokai_style = "shusia";
+
+      # substitution live preview
+      inccomand = "nosplit";
+
+      # WARNING: blah blah this is experimental
+      #          if you encounter issues, disable this
+      # NOTE: seems to not be working atm anyway :)
+      cmdheight = 0;
+    };
+
+
+    colorscheme = "sonokai";
+
+    opts = {
+      number = true;
+      relativenumber = true;
+
+      breakindent = true;
+
+      # save undo history
+      undofile = true;
+
+      # case-insensitive search unless specified
+      ignorecase = true;
+      smartcase = true;
+
+      # generally enables mouse stuff
+      mouse = "a";
+
+      # use system clipboard
+      clipboard = "unnamedplus";
+
+      # decrease update time
+      updatetime = 250;
+
+      # minimum margin lines around cursor
+      scrolloff = 5;
+
+      # mode is shown in lualine
+      showmode = false;
+
+      # linebreak so that words aren't split when wrap
+      linebreak = true;
+    };
+
+    extraConfigLua = ''
+      --vim.g.sonokai_style = 'shusia'
+      --vim.cmd([[colorscheme sonokai]])
+      
+      vim.o.guifont = "${builtins.elemAt fonts.fontconfig.defaultFonts.monospace 0}:h12"
+      
+      if vim.g.neovide then
+        vim.g.neovide_hide_mouse_when_typing = true
+        vim.g.neovide_remember_window_size = true
+        vim.g.neovide_cursor_animate_in_insert_mode = false
+        vim.g.neovide_cursor_vfx_mode = "railgun"
+      end
+    '';
+
+    editorconfig.enable = true;
+
+    keymaps = [
+      {
+        key = "<Esc><Esc>";
+        action = "<C-\\><C-n>";
+        mode = "t";
+      }
+    ];
+
+    ## use system clipboard
+    #clipboard.register = "unnamedplus";
+
+    plugins = rec {
+
+      lsp = {
+        enable = true;
+        servers = {
+          nil_ls = {
+            enable = true;
+            extraOptions = {
+              formatting.command = "${pkgs.nixpkgs-fmt}";
+            };
+          };
+          lua-ls.enable = true;
+          pyright.enable = true;
+        };
+      };
+
+      lsp-format.enable = true;
+
+      lspkind = {
+        enable = true;
+        cmp.enable = cmp.enable;
+      };
+
+      #lspsaga = {
+      #  enable = true;
+
+      #  # breadcrumbs
+      #  symbolInWinbar.enable = true;
+      #};
+
+      trouble.enable = true;
+
+      # detect tab/spaces and width
+      sleuth.enable = true;
+
+      treesitter.enable = true;
+
+      treesitter-context = {
+        enable = true;
+        settings = rec {
+          max_lines = 6;
+          min_window_height = 3 * max_lines;
+        };
+      };
+
+      treesitter-refactor = {
+        enable = true;
+        highlightDefinitions.enable = true;
+
+        # bound to 'grr' by default
+        smartRename.enable = true;
+      };
+
+      nvim-tree = {
+        enable = true;
+        syncRootWithCwd = true;
+
+        # colours are enough imo
+        renderer.icons.show.git = false;
+      };
+
+      # a lua powered greeter like vim-startify / dashboard-nvim
+      alpha = {
+        enable = true;
+        layout = [
+          {
+            type = "text";
+            val = [
+              "hello gays welcome to my nixvim config"
+              ""
+              "⠀⠀⠀⢰⠶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⠶⠲⣄⠀"
+              "⠀⠀⣠⡟⠀⠈⠙⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⡶⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠾⠋⠁⠀⠀⢽⡄"
+              "⠀⠀⡿⠀⠀⠀⠀⠀⠉⠷⣄⣀⣤⠤⠤⠤⠤⢤⣷⡀⠙⢷⡄⠀⠀⠀⠀⣠⠞⠉⠀⠀⠀⠀⠀⠈⡇"
+              "⠀⢰⡇⠀⠀⠀⠀⠀⠀⠀⠉⠳⣄⠀⠀⠀⠀⠀⠈⠁⠀⠀⠹⣦⠀⣠⡞⠁⠀⠀⠀⠀⠀⠀⠀⠀⡗"
+              "⠀⣾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣻⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣏"
+              "⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡇"
+              "⠀⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠂"
+              "⠀⢿⠀⠀⠀⠀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣤⣤⡀⠀⠀⠀⠀⠀⣸⠇⠀"
+              "⠀⠘⣇⠀⠀⠀⠀⠉⠉⠛⠛⢿⣶⣦⠀⠀⠀⠀⠀⠀⢴⣾⣟⣛⡋⠋⠉⠉⠁⠀⠀⠀⠀⣴⠏⠀⠀"
+              "⢀⣀⠙⢷⡄⠀⠀⣀⣤⣶⣾⠿⠋⠁⠀⢴⠶⠶⠄⠀⠀⠉⠙⠻⠿⣿⣷⣶⡄⠀⠀⡴⠾⠛⠛⣹⠇"
+              "⢸⡍⠉⠉⠉⠀⠀⠈⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⣬⠷⣆⣠⡤⠄⢀⣤⠞⠁⠀"
+              "⠈⠻⣆⡀⠶⢻⣇⡴⠖⠀⠀⠀⣴⡀⣀⡴⠚⠳⠦⣤⣤⠾⠀⠀⠀⠀⠀⠘⠟⠋⠀⠀⠀⢻⣄⠀⠀"
+              "⠀⠀⣼⠃⠀⠀⠉⠁⠀⠀⠀⠀⠈⠉⢻⡆⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⠀"
+              "⠀⢠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡀⠀⠀⢀⡇⠀⠀⠀⠀⠀⠀⠀⠀⣀⡿⠧⠿⠿⠟⠀⠀"
+              "⠀⣾⡴⠖⠛⠳⢦⣿⣶⣄⣀⠀⠀⠀⠀⠘⢷⣀⠀⣸⠃⠀⠀⠀⣀⣀⣤⠶⠚⠉⠀⠀⠀⠀⠀⠀⠀"
+              "⠀⠀⠀⠀⠀⠀⠀⠈⢷⡀⠈⠻⠦⠀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠹⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+              "⠀⠀⠀⠀⠀⠀⠀⢀⡴⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀"
+              "⠀⠀⠀⠀⠀⠀⢠⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⡄⠀⠀⠀⠀⠀⠀⠀⠀"
+              "⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⢲⡗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀"
+              "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀"
+              "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠋⠀⠀⠀⠀⠀⠀⠀"
+              ""
+              "this is a read-only buffer, sorry"
+            ];
+          }
+        ];
+
+        #theme = "dashboard";
+      };
+
+      #coq-nvim = {
+      #  enable = true;
+      #  installArtifacts = true;
+      #  settings.auto_start = "shut-up";
+      #};
+
+      cmp.enable = true;
+
+      cmp-path.enable = true;
+      cmp-nvim-lsp.enable = true;
+      cmp-nvim-lsp-document-symbol.enable = true;
+      cmp-nvim-lsp-signature-help.enable = true;
+      cmp-treesitter.enable = true;
+
+      fidget.enable = true;
+
+      #cursorline = {
+      #  enable = true;
+      #  cursorline.enable = false;
+      #  cursorword.enable = true;
+      #};
+
+      lualine = {
+        enable = true;
+        globalstatus = true;
+
+        sections = {
+          lualine_x = [
+            "searchcount"
+            "filetype"
+          ];
+        };
+
+        sectionSeparators = {
+          left = "";
+          right = "";
+        };
+
+        componentSeparators = {
+          left = "";
+          right = "";
+        };
+      };
+    };
+  };
 }
