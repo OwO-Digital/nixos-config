cba34d6400c61fb767e00c2624433b7c2ebd5455
Octelly <octelly@owo.digital>
2024-12-07 23:24:35 +0100

    pain

--- packages/misc/mqtt_linux/mqtt_linux/__init__.py
+++ packages/misc/mqtt_linux/mqtt_linux/__init__.py
@@ -1,10 +1,49 @@
-from monitorcontrol import get_monitors
+import json
+from ha_mqtt_discoverable import Settings, DeviceInfo
+from ha_mqtt_discoverable.sensors import Light, LightInfo
+from paho.mqtt.client import Client, MQTTMessage
 
+# Configure the required parameters for the MQTT broker
+mqtt_settings = Settings.MQTT(host = "192.168.1.103")
+
+device_info = DeviceInfo(name="desktop", identifiers="foo1bar2")
+
+# Information about the light
+light_info = LightInfo(
+    name="brightness",
+    unique_id="bar3foo4",
+    device=device_info,
+    brightness=True,
+)
+
+settings = Settings(mqtt=mqtt_settings, entity=light_info)
+
+# To receive state commands from HA, define a callback function:
+def my_callback(client: Client, user_data, message: MQTTMessage):
+
+    # Make sure received payload is json
+    try:
+        payload = json.loads(message.payload.decode())
+    except ValueError as error:
+        print("Ony JSON schema is supported for light entities!")
+        return
+
+    # Parse received dictionary
+    if "brightness" in payload:
+        print(payload["brightness"])
+        my_light.brightness(payload["brightness"])
+    elif "state" in payload:
+        if payload["state"] == light_info.payload_on:
+            print("on")
+            my_light.on()
+        else:
+            print("off")
+            my_light.off()
+    else:
+        print("Unknown payload")
+
+
+my_light = Light(settings, my_callback)
 
 def app() -> None:
-    for monitor in get_monitors():
-        with monitor:
-            try:
-                print(monitor.get_luminance())
-            except Exception as e:
-                print(e)
+    my_light.off()
