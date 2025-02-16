217226f89b754f31d08813fb9c873477728d8da4
Octelly <octelly@owo.digital>
2025-02-17 00:47:12 +0100

    apro+octelly: added emi's desktop

---
+++ hosts/mksft-desktop/configuration.nix
@@ -0,0 +1,71 @@
+{ config, pkgs, ... }: {
+  modules = {
+    desktop = {
+      gaming = {
+        steam.enable = true;
+        itch.enable = true;
+        olympus.enable = true;
+        minecraft.enable = true;
+        sunshine.enable = true;
+        utils.overlays.vkbasalt = true;
+        emulation = {
+          enable = true;
+          nintendo = {
+            switch.ryujinx = true;
+            wii = true;
+          };
+        };
+      };
+      awesome = {
+        enable = true;
+        package = pkgs.awesome-git;
+      };
+      #hyprland.enable = false;
+      # sway.enable = true;
+      # river.enable = true;
+    };
+    #hardware.laptop.enable = true;
+    system = {
+      sound.enable = true;
+      xorg = {
+        enable = true;
+        #layout = "fck";
+      };
+    };
+  };
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      # p sure this was here only for Davinki
+      #rocmPackages.clr.icd
+    ];
+  };
+
+  programs.corectrl.enable = true;
+
+  virtualisation.vmware.host = {
+    enable = true;
+    extraConfig = ''
+      # Allow unsupported device's OpenGL and Vulkan acceleration for guest vGPU
+      mks.gl.allowUnsupportedDrivers = "TRUE"
+      mks.vk.allowUnsupportedDevices = "TRUE"
+    '';
+  };
+
+  services.flatpak.enable = true;
+
+  services.btrfs.autoScrub = {
+    enable = true;
+    interval = "weekly";
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    25565 # minecra
+    3216 # EA App
+  ];
+  networking.firewall.allowedUDPPorts = [
+    25565 # minecra
+    3216 # EA App
+  ];
+}
---
+++ hosts/mksft-desktop/default.nix
@@ -0,0 +1,31 @@
+{ inputs, lib, repoConf, ... }:
+
+let system = "x86_64-linux"; in
+lib.nixosSystem {
+  inherit system;
+
+  modules = [
+    {
+      nixpkgs = repoConf;
+      networking.hostName = "mksft-desktop";
+    }
+
+    inputs.home.nixosModules.home-manager
+    inputs.flatpaks.nixosModules.declarative-flatpak
+    #inputs.nixos-cosmic.nixosModules.default
+
+    inputs.nixHW.nixosModules.common-cpu-amd
+    inputs.nixHW.nixosModules.common-cpu-amd-pstate
+    inputs.nixHW.nixosModules.common-cpu-amd-zenpower
+    inputs.nixHW.nixosModules.common-gpu-amd
+    inputs.nixHW.nixosModules.common-pc
+    inputs.nixHW.nixosModules.common-pc-ssd
+
+    ../shared/configuration.nix
+    ./configuration.nix
+    ./hardware-configuration.nix
+  ] ++
+  (lib.mapModulesRec' ../../modules import);
+
+  specialArgs = { inherit inputs system; };
+}
