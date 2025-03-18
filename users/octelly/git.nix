9444bf5b298098cfbb0f97fe473ca4022f1fd16d
Octelly <octelly@owo.digital>
2025-03-18 16:49:16 +0100

    octelly: github-related git fixes

--- users/octelly/git.nix
+++ users/octelly/git.nix
@@ -1,13 +1,27 @@
-{ ... }:
+{ pkgs, lib, ... }:
+let
+  use_gh = true;
+in
 {
   programs.git = {
     enable = true;
 
     userName = "Octelly";
     userEmail = "octelly@owo.digital";
+    extraConfig.credential = {
+      "https://github.com" = lib.mkIf use_gh {
+        helper = "${pkgs.gh}/bin/gh auth git-credential";
+      };
+    };
   };
+
+  #programs.gh = {
+  #  enable = true;
+  #};
+
+  home.packages = lib.optional use_gh pkgs.gh;
 
   # log in to GitHub and other popular hosts
   # without setup
-  programs.git-credential-oauth.enable = true;
+  #programs.git-credential-oauth.enable = true;
 }
