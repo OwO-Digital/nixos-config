059c16b7c560df4ff740d946e813ed799c69d38a
Octelly <octelly@owo.digital>
2025-05-02 12:30:50 +0200

    comment out plugins that break system rebuilds (ended up slowly adding all of them over time)

--- users/apro/programs/wms/hyprland/plugins.nix
+++ users/apro/programs/wms/hyprland/plugins.nix
@@ -1,53 +1,60 @@
 { config, inputs, lib, pkgs, system, ... }: {
 
-	wayland.windowManager.hyprland = {
-		plugins = (with inputs.hyprland-plugins.packages.${system}; [
-			# hyprbars #crashes with hyprtrails but hyprbars suck anyways
-			hyprtrails #crashes with hyprbars
-			hyprexpo
-		]) ++ [
-			inputs.hyprsplit.packages.${system}.hyprsplit
-			inputs.hypr-dynamic-cursors.packages.${system}.hypr-dynamic-cursors
-		];
+  wayland.windowManager.hyprland = {
+    #plugins = (with inputs.hyprland-plugins.packages.${system}; [
+    #  # hyprbars #crashes with hyprtrails but hyprbars suck anyways
 
-		settings = {
-			plugin = {
-				hyprsplit.num_workspaces = "6";
+    #  #hyprtrails #crashes with hyprbars
+    #  # elly: prevents system update
 
-				dynamic-cursors = {
-					enabled = "true";
-					mode = "tilt";
+    #  #hyprexpo
+    #  # elly: prevents system update
+    #]) ++ [
+    #  # inputs.hyprsplit.packages.${system}.hyprsplit
+    #  # elly: prevents system update
 
-					rotate = {
-						length = "24";
-						offset = "0.0";
-					};
+    #  #inputs.hypr-dynamic-cursors.packages.${system}.hypr-dynamic-cursors
+    #  # elly: prevents system update
+    #];
 
-					shake.enabled = "true";
-				};
+    settings = {
+      plugin = {
+        hyprsplit.num_workspaces = "6";
 
-				hyprbars = {
-					bar_height = "24";
-					bar_color  = "rgb(141b1e)";
-					"col.text" = "rgb(dadada)";
-					bar_text_size = "12";
-					bar_text_font = "Roboto Condensed";
-					hyprbars-button = [
-						"rgb(e57474), 12, x, hyprctl dispatch killactive"
-						"rgb(e5c76b), 12, o, hyprctl dispatch fullscreen 1"
-						"rgb(8ccf7e), 12, m, hyprctl dispatch swapwithmaster"
-					];
-				};
+        dynamic-cursors = {
+          enabled = "true";
+          mode = "tilt";
 
-				hyprtrails.color = "rgba(67b0e880)";
+          rotate = {
+            length = "24";
+            offset = "0.0";
+          };
 
-				hyprexpo = {
-					columns = "3";
-					gap_size = "32";
-					bg_col = "rgb(141b1e)";
-					workspace_method = "first 1";
-				};
-			};
-		};
-	};
+          shake.enabled = "true";
+        };
+
+        hyprbars = {
+          bar_height = "24";
+          bar_color = "rgb(141b1e)";
+          "col.text" = "rgb(dadada)";
+          bar_text_size = "12";
+          bar_text_font = "Roboto Condensed";
+          hyprbars-button = [
+            "rgb(e57474), 12, x, hyprctl dispatch killactive"
+            "rgb(e5c76b), 12, o, hyprctl dispatch fullscreen 1"
+            "rgb(8ccf7e), 12, m, hyprctl dispatch swapwithmaster"
+          ];
+        };
+
+        hyprtrails.color = "rgba(67b0e880)";
+
+        hyprexpo = {
+          columns = "3";
+          gap_size = "32";
+          bg_col = "rgb(141b1e)";
+          workspace_method = "first 1";
+        };
+      };
+    };
+  };
 }
