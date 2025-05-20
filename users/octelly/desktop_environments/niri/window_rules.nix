c2eaee7662c6e509c53a114d51e81bcc4fecb419
Octelly <octelly@owo.digital>
2025-05-20 18:13:11 +0200

    octelly: niri steam rules

--- users/octelly/desktop_environments/niri/window_rules.nix
+++ users/octelly/desktop_environments/niri/window_rules.nix
@@ -4,7 +4,13 @@
     {
       # open non-main Steam windows as floating
       matches = [{ app-id = "steam"; }];
-      excludes = [{ title = "Steam"; }];
+      excludes = [{
+        # Regex:
+        # ^ is the start of the string
+        # $ is the end of the string
+        # (this makes it not apply to e.g. "Steam Settings")
+        title = "^Steam$";
+      }];
       open-floating = true;
     }
   ];
